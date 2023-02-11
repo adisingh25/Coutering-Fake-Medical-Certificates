da80f81b8c481223d5294b76a6f51c8491981099
Pawan Kumar <kpawan1009@gmail.com>
2023-02-11 22:07:41 +0530

    form verification

--- APP/home/forms.py
+++ APP/home/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
+from .models import CERTIFICATE
 
-from home.models import CERTIFICATE
 class CertificateForm(ModelForm):
     class Meta:
         model = CERTIFICATE
--- APP/home/urls.py
+++ APP/home/urls.py
@@ -1,19 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render,HttpResponse,redirect
-from home.views import home,doctor,patient,signup,applyCertificate,signout
-
-
-
-
-
+from .views import home,doctor,patient,signup,applyCertificate,signout,acceptApplication
 
 urlpatterns = [ 
    path('', home, name='home'),
    path('doctor/', doctor, name='doctor'),
    path('patient/', patient, name='patient'),
    path('signup/', signup),
-   path('apply/', applyCertificate, name='applyCertificate'),
+   path('apply/',applyCertificate,name='applyCertificate'),
+   path('acceptApplication/', acceptApplication, name='acceptApplication'),
+
    path('logout/', signout),
 ]
